b76b2304704d0386de76fbe5531db230722a16f5
strbakovalivia <strbakovalivia@gmail.com>
2026-02-15 22:15:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 
 # --- NASTAVENÍ STRÁNKY ---
 st.set_page_config(
-    page_title="StepQueen 🏃‍♀️", 
+    page_title="StepQueen Challenge", # Tohle pomůže vyhledávačům a systému
     page_icon="👑",
     layout="centered",
     initial_sidebar_state="collapsed"
